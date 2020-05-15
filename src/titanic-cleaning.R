b7272cb53859023a9bcd93afb41321eaea5af797
Gabriel <g.izquierdoalcaraz@gmail.com>
2020-05-15 20:57:04 +0200

    :sparkles: Concat train and test data

--- src/titanic-cleaning.R
+++ src/titanic-cleaning.R
@@ -6,4 +6,4 @@ titanic_train <- read.csv("../data/train.csv")
 titanic_test <- read.csv("../data/test.csv")
 titanic_gender_submission <- read.csv("../data/gender_submission.csv")
 titanic_test <- merge(titanic_test, titanic_gender_submission, by="PassengerId")
-
+titanic_data <- rbind(titanic_train, titanic_test)
